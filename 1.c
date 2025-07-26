d725aa91b64e1e35ae6081ce7709cb5a8d73ac00
Ziyovuddin <valijonov@ziyovuddin.com>
2025-07-26 15:04:17 +0500

    qoshish

--- 1.c
+++ 1.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
-#include <math.h>
+int main()
+{
+long long n;
+printf("Input: ");
+scanf("%lld", &n);
+long long sum = 0;
+long long tn = n;
+while (tn > 0) {
+int digit = tn % 10;
+if (digit % 2 == 0) {
+sum += (long long)digit * digit * digit;
+}
+tn /= 10;
+}
+printf("Output: %lld\n", sum);
+return 0;
+}
